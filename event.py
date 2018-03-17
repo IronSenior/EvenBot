a23e90e30c938733caa674f7f0531b7b64bdf8ac
Iván Portillo <ivanportillo@me.com>
2018-03-17 12:05:03 +0100

    Create event class

---
+++ event.py
@@ -0,0 +1,69 @@
+class Event:
+    def __init__(self, date, tag, name, group, locX, locY, locZ, desc, image):
+        self.date = date
+        self.tag = tag
+        self.name = name
+        self.group = group
+        self.locX = locX
+        self.locY = locY
+        self.locZ = locZ
+        self.desc = desc
+        self.image = image
+
+    @property
+    def date(self):
+        return self.date
+
+    @property
+    def tag(self):
+        return self.tag
+
+    @property
+    def name(self):
+        return self.name
+
+    @property
+    def group(self):
+        return self.group
+
+    @property
+    def location(self):
+        return self.locX, self.locY, self.locZ
+
+    @property
+    def description(self):
+        return self.desc
+
+    @property
+    def image(self):
+        return self.image
+
+    @date.setter
+    def date(self, date):
+        self.date = date
+
+    @tag.setter
+    def tag(self, tag):
+        self.tag = tag
+
+    @name.setter
+    def name(self, name):
+        self.name = name
+
+    @group.setter
+    def group(self, group):
+        self.group = group
+
+    @location.setter
+    def location(self, x, y, z):
+        self.locX = x
+        self.locY = y
+        self.locZ = z
+
+    @description.setter
+    def description(self, description):
+        self.desc = description
+
+    @image.setter
+    def image(self, image):
+        self.image = image
