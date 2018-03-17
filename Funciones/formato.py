08b84b17a9b07f9318792db08cd899fe894ed453
Iván Portillo <ivanportillo@me.com>
2018-03-17 17:50:11 +0100

    Fix es_link function

--- Funciones/formato.py
+++ Funciones/formato.py
@@ -1,15 +1,16 @@
 def es_fecha(texto):
-	try:
-		msg = texto.split(" ")
-		fecha = msg[0]
-		hora = msg[1]
+    try:
+        msg = texto.split(" ")
+        fecha = msg[0]
+        hora = msg[1]
 
-		if (fecha[4] == "-") and (fecha[7] == "-") and (hora[2] == ":"):
-			return True
+        if (fecha[4] == "-") and (fecha[7] == "-") and (hora[2] == ":"):
+            return True
 
-	except:
-		return False
+    except:
+        return False
+
 
 def es_link(link):
-	if link[:1] == "t.":
-		return True
+    if link[:2] == "t.":
+        return True
